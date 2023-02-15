82bc2b1c11b7091a4fcff4a929b9b381494b9403
vdelarue <vdelarue@umich.edu>
2023-02-15 16:26:18 -0500

    Added radio button features for bike

--- js/barchart.js
+++ js/barchart.js
@@ -3,6 +3,8 @@ const width = 800,
       height = 400,
       margin = {top: 20, right: 50, bottom: 100, left: 60};
 
+var direction = "both";
+
 const innerWidth = width - margin.left - margin.right, 
       innerHeight = height - margin.top - margin.bottom;
 
@@ -34,92 +36,154 @@ function updateChart(){
 
 
 // get the data
-d3.csv("./data/counter-data-resampled/counter-data-1hour.csv").then(function(data) {
+  d3.csv("./data/counter-data-resampled/counter-data-1hour.csv").then(function(data) {
 
-  // format the data
-  data.forEach(function(d) {
-    d.parsedDateTime = parseTime(d.datetime);
-    d.in = +d.in;
-    d.out = +d.out;
-    d.total = d.in + d.out;
-  });
+    // format the data
+    data.forEach(function(d) {
+      d.parsedDateTime = parseTime(d.datetime);
+      d.in = +d.in;
+      d.out = +d.out;
+      d.total = d.in + d.out;
+    });
 
-  console.log(start_date)
+    console.log(start_date)
+      data = data.filter(function(d) {
+      return parseDate(start_date) <= d.parsedDateTime
+    });
+
     data = data.filter(function(d) {
-    return parseDate(start_date) <= d.parsedDateTime
-  });
-
-  data = data.filter(function(d) {
-    return d.parsedDateTime <= d3.timeDay.offset(parseDate(end_date), 1)
-  });
-
-
-//   // specify the (input) domains
-//  xScale = d3.scaleTime()
-//   .domain(d3.extent(data, d=>d.parsedDateTime))
-//   .range([0, innerWidth]), 0.05;
-
-// yScale = d3.scaleLinear()
-//   .domain([0, d3.max(data, d=>d.total)])
-//   .range([innerHeight, 0]);
+      return d.parsedDateTime <= d3.timeDay.offset(parseDate(end_date), 1)
+    });
 
     // specify the (input) domains
     xScale.domain(d3.extent(data, d=>d.parsedDateTime));
-    yScale.domain([0, d3.max(data, d=>d.total)]);
 
+    if (direction == "north") {
+      // set the y scale to in
+      yScale.domain([0, d3.max(data, d=>d.in)]);
+    
+      // create bars given x data is in
+      svg.selectAll("bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function(d) {
+              return xScale(d.parsedDateTime);
+          })
+        .attr("y", function(d) {
+            return yScale(d.in);
+          })
+        .attr("width", ((width/data.length) - (0.15 * width/data.length)))
+        .attr("height", function(d) {
+            return innerHeight - yScale(d.in);
+          })
+        .attr("fill", function(d) {
+          return "rgb(106, 106, 246)";
+      });
+    }
 
-svg.selectAll("bar")
+    else if (direction == "south") {
+      // set the y scale to out
+      yScale.domain([0, d3.max(data, d=>d.out)]);
+
+      svg.selectAll("bar")
       .data(data)
       .enter()
       .append("rect")
       .attr("x", function(d) {
             return xScale(d.parsedDateTime);
-         })
-        .attr("y", function(d) {
-           return yScale(d.total);
-         })
-        .attr("width", ((width/data.length) - (0.15 * width/data.length)))
-        .attr("height", function(d) {
-            return innerHeight - yScale(d.total);
-          })
-         .attr("fill", function(d) {
-           return "rgb(91, 121, 28)";
-         });
+        })
+      .attr("y", function(d) {
+          return yScale(d.out);
+        })
+      .attr("width", ((width/data.length) - (0.15 * width/data.length)))
+      .attr("height", function(d) {
+          return innerHeight - yScale(d.out);
+        })
+      .attr("fill", function(d) {
+        return "rgb(220, 183, 55)";
+      });
+    }
 
+    else if (direction == "both") {
+        // set the y scale to total
+        yScale.domain([0, d3.max(data, d=>d.total)]);
+        svg.selectAll("bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) {
+                  return xScale(d.parsedDateTime);
+              })
+            .attr("y", function(d) {
+                return yScale(d.total);
+              })
+            .attr("width", ((width/data.length) - (0.15 * width/data.length)))
+            .attr("height", function(d) {
+                return innerHeight - yScale(d.total);
+              })
+            .attr("fill", function(d) {
+              return "rgb(91, 121, 28)";
+        });
+        // add x axis
+    }
 
-// add x axis
-svg.append("g")
-  .attr("class", "x-axis")
-  .attr("transform", "translate(0," + innerHeight + ")")
-  .call(d3.axisBottom(xScale)
-  );  
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0," + innerHeight + ")")
+      .call(d3.axisBottom(xScale)
+    );  
 
-// add y axis
-svg.append("g")
-  .attr("class", "y-axis")    
-  .call(d3.axisLeft(yScale)
-  );
+    // add y axis
+    svg.append("g")
+      .attr("class", "y-axis")    
+      .call(d3.axisLeft(yScale)
+    );
+        
+  });
 
-
-});
-
-// add x label
-svg.append("text")
-  .attr("class", "axis-label")
-  .attr("x", innerWidth/2)
-  .attr("y", innerHeight+50)
-  .style("text-anchor", "middle")
-  .text("Date & time");
-
-// add y label
-svg.append("text")
-  .attr("class", "axis-label")
-  .attr("transform", "rotate(-90)")
-  .attr("y", -30)    // offset the label position
-  .attr("x", -innerHeight/2)
-  .style("text-anchor", "middle")
-  .text("Count");
-
+  // add x label
+  svg.append("text")
+    .attr("class", "axis-label")
+    .attr("x", innerWidth/2)
+    .attr("y", innerHeight+50)
+    .style("text-anchor", "middle")
+    .text("Date & time");
+  // add y label
+  svg.append("text")
+    .attr("class", "axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -30)    // offset the label position
+    .attr("x", -innerHeight/2)
+    .style("text-anchor", "middle")
+    .text("Count");
 }
 
 updateChart();
+
+// executes when a button is clicked
+d3.selectAll("input")
+  .on("click", function() {
+
+    var view = d3.select(this).node().value;
+
+    switch (view) {
+
+      case "north":
+      direction = "north";
+      updateChart();
+      break;
+      
+      case "south":
+        direction = "south";
+        updateChart();
+        break;
+      
+      case "both":
+        direction = "both";
+        updateChart();
+        break;
+
+      default:
+    }
+});
