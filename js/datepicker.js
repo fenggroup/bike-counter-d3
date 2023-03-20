9fb538d77029a5daa6187fdc27089af3431e5192
vdelarue <vdelarue@umich.edu>
2023-03-20 08:06:53 -0400

    Changed formatting to accomodate table

--- js/datepicker.js
+++ js/datepicker.js
@@ -5,14 +5,16 @@ $('.input-daterange').datepicker({
     format: 'yyyy-mm-dd',
     startDate: start_date,
     endDate: end_date,
-    orientation: 'bottom',
-    // autoclose: true,
+    orientation: 'bottom'
+    //autoclose: true,
 })
 .on("changeDate", function() {
-
     start_date = document.getElementsByName('start')[0].value;
     end_date = document.getElementsByName('end')[0].value;
 
     updateChart();
+    console.log("exit from barchart");
 
+    updateTable();
+    console.log("exit from table");
 });
