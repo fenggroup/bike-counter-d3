889467d62ba05234ca5020d9751c6a9ced9921d1
vdelarue <vdelarue@umich.edu>
2023-03-29 23:08:28 -0400

    right oriented values and changed decimal place to one

--- js/table.js
+++ js/table.js
@@ -1,16 +1,14 @@
-// set default variables to what is loaded in
-const tableFile = "./data/counter-data-resampled/counter-data-1day.csv";
 
+const oneDayFile = "./data/counter-data-resampled/counter-data-1day.csv";
 
 // create the initial table
 updateTable();
-console.log("test 15");
 
 // get the data
 function updateTable() {
-    d3.selectAll("table, thead, tbody, tr, td").remove();
+    d3.selectAll("#table table").remove();
     
-    d3.csv(tableFile).then(function(data) {
+    d3.csv(oneDayFile).then(function(data) {
         // format the data
         data.forEach(function(d) {
             d.parsedDateTime = parseTime(d.date);
@@ -28,12 +26,9 @@ function updateTable() {
 
         createTable(data);
     });
-
-    console.log("test 14");
 }
 
 function createTable(data) {
-    console.log("test 1");
 
     var totalTraffic = {
         both: d3.sum(data, d => d.both),
@@ -41,15 +36,12 @@ function createTable(data) {
         out: d3.sum(data, d => d.out)
     };
 
-    console.log("test 2");
-
     var averageDailyTraffic = {
         both: totalTraffic.both / data.length,
         in: totalTraffic.in / data.length,
         out: totalTraffic.out / data.length
     };
 
-    console.log("test 3");
 
     var percentTraffic = {
         both: 100,
@@ -57,18 +49,15 @@ function createTable(data) {
         out: (totalTraffic.out / totalTraffic.both) * 100
     };
 
-    console.log("test 4");
 
     var summaryData = [
         { label: "Both", total: totalTraffic.both, average: averageDailyTraffic.both, percent: percentTraffic.both },
         { label: "In", total: totalTraffic.in, average: averageDailyTraffic.in, percent: percentTraffic.in },
         { label: "Out", total: totalTraffic.out, average: averageDailyTraffic.out, percent: percentTraffic.out }
     ];
-    console.log("test 5");
 
     var tableContainer = d3.select("#table")
     
-    console.log("test 6");
 
     var table = tableContainer.append("table")
         .style("margin-left", "auto")
@@ -81,40 +70,33 @@ function createTable(data) {
     var thead = table.append("thead");
     var tbody = table.append("tbody");
 
-    console.log("test 7");
-
     thead.append("tr")
         .selectAll("th")
         .data(["", "Total traffic", "Average daily traffic", "Percent"])
         .enter()
         .append("th")
-        .text(d => d);
-    console.log("test 8");
-
+        .text(d => d)
+        .style("text-align", "right");
 
     var rows = tbody.selectAll("tr")
         .data(summaryData)
         .enter()
         .append("tr");
-    console.log("test 9");
 
-    
     rows.append("td")
         .text(d => d.label)
         .style("font-weight", "bold");
-    console.log("test 10");
-
-    
-    rows.append("td")
-        .text(d => d.total);
-    console.log("test 11");
 
     rows.append("td")
-        .text(d => d.average.toFixed(2));
-    console.log("test 12");
+        .text(d => d3.format(",")(d.total))
+        .style("text-align", "right");
 
     rows.append("td")
-        .text(d => d.percent.toFixed(1) + "%");
-    console.log("test 13");
+        .text(d => d3.format(",.1f")(d.average))
+        .style("text-align", "right");
+
+    rows.append("td")
+        .text(d => d.percent.toFixed(1) + "%")
+        .style("text-align", "right");
 
 }
